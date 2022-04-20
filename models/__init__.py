b1aed30283650a330a9a35e2aebf89c2d080534b
Daniel Santibáñez Polanco <dansanti@gmail.com>
2022-04-20 12:11:01 -0400

    [IMP]Inspección de folios

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 from . import account_move
-from . import stock_location
-from . import stock_picking
 from . import dte
-from . import libro
+from . import dte_caf
 from . import ir_sequence
+from . import libro
+from . import procurement
 from . import purchase_order
 from . import sale_order
-from . import procurement
 from . import sii_xml_envio
+from . import stock_location
+from . import stock_picking
