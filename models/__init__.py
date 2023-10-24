55c2b77814662b601cb02dc1752f9cc95c97cf36
Cesar250101 <cesar250101@outlook.com>
2023-10-24 11:52:31 -0300

    first commit

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from . import account_invoice
+from . import dte_caf
+from . import libro
+from . import ir_module_module
+from . import ir_sequence
+from . import purchase_order
+from . import procurement
+from . import sale_order
+from . import sii_xml_envio
+from . import stock_picking
+from . import stock_move
+from . import stock_warehouse
