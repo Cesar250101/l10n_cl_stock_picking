5300a884078788c86ba1c0e29e37b574e5f39f27
Daniel Santibáñez Polanco <dansanti@gmail.com>
2024-03-25 09:54:43 -0300

    [IMP]name desde SO

--- models/sale_order.py
+++ models/sale_order.py
@@ -8,11 +8,12 @@ _logger = logging.getLogger(__name__)
 class SOL(models.Model):
     _inherit = 'sale.order.line'
 
-    
+
     def _prepare_procurement_values(self, group_id=False):
         result = super(SOL, self)._prepare_procurement_values(group_id)
         self.ensure_one()
         result.update({
+                'name': self.name,
                 'precio_unitario': self.price_unit,
                 'discount': self.discount,
                 'move_line_tax_ids': self.tax_id.ids,
