dc10b5944b63f7087e3171701a0ba5daf9481411
Diego Ramirez <rinukdrive@gmail.com>
2024-07-04 22:59:35 +0000

    Update file account_move.py

--- models/account_move.py
+++ models/account_move.py
@@ -66,8 +66,8 @@ class PickingToInvoiceD(models.Model):
             sp = False
             if inv.is_invoice():
                 for ref in inv.referencias:
-                    if ref.sii_referencia_TpoDocRef.sii_code in [ 56 ]:
-                        sp = self.env['stock_picking'].search([
+                    if ref.sii_referencia_TpoDocRef.sii_code in [ 52 ]:
+                        sp = self.env['stock.picking'].search([
                             ('sii_document_number', '=', ref.origen)])
                 if sp:
                     if inv.move_type in ['out_invoice']:
